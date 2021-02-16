08bda3c72c6072018a05d82e2fd33cbb764d93c0
‪alaadeeb deeb95‬‏ <alaadeebdeeb9955@gmail.com>
2021-02-17 00:55:27 +0200

    sec-try

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -25,6 +25,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 const appendTheEnd = (str) => {
   // Solution code here...
   let stringEnd = str + ' The end.';
+  return stringEnd
 };
 
 /* ------------------------------------------------------------------------------------------------
